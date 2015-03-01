a9b7eaaff2a32abca653674f111ee12f2681f15c
100mango <100mango@users.noreply.github.com>
2015-03-01 20:36:47 +0800

    add quick sort

---
+++ quickSort.py
@@ -0,0 +1,24 @@
+import random
+
+def quickSort(array):
+	less = []
+	equal = []
+	greatter = []
+
+	if len(array) < 1:
+		return array
+	else:
+		pivot = array[0]    #random.choice(array) 更好
+		for element in array:
+			if element < pivot:
+				less.append(element)
+			if element == pivot:
+				equal.append(element)
+			if element > pivot:
+				greatter.append(element)
+		return quickSort(less) + equal + quickSort(greatter)
+
+
+sortArray =  quickSort([2,3,1,43,54,2,45,67,11,2,4,6,7,8])
+
+print sortArray
