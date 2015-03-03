4df27c7af4777786cbad5bf4008096e5e5ff2f99
100mango <100mango@users.noreply.github.com>
2015-03-03 23:30:11 +0800

    add binary search tree

---
+++ binarySearchTree.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+
+class Node:
+
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+
+	def insert(self,data):
+		if self.data == None:
+			self.data = data
+		else
+			if data < self.data:
+				if self.left is None:
+					self.left = Node(data)
+				else
+					self.left.insert(data)
+			elif data > self.data:
+				if self.right is None:
+					self.right = Node(data)
+				else
+					self.right = Node(data)
+
