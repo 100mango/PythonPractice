ed35df2c0df42cb1ff3a107272d9b527a6106817
100mango <100mango@users.noreply.github.com>
2015-04-14 16:45:03 +0800

    add single number 1 2

--- bitManipulation.py
+++ bitManipulation.py
@@ -1,4 +1,5 @@
 #coding=utf8
+
 #Number of 1 Bits
 #Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
 #For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
@@ -15,3 +16,28 @@ class Solution:
     		count = count + 1
     		n = n & (n-1)
     	return count
+
+#leetcode single number
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A): #翻硬币思路，遇到一次就翻一次，则最后只有一个是反面
+        result = 0
+        for number in A:
+            result = result ^ number
+        return result
+     
+#leetcode Single Number II        
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        dictionary = {}
+        for number in A:
+            if number not in dictionary:
+                dictionary[number] = 1
+            else:
+                dictionary[number] = dictionary[number] + 1
+        for number in dictionary:
+            if dictionary[number] == 1:
+                return number
