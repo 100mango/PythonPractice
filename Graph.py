c325e0866981c4fbfcb7e97e1739fb372460fe7b
100mango <100mango@users.noreply.github.com>
2015-06-11 19:25:53 +0800

    add dfs

--- Graph.py
+++ Graph.py
@@ -9,9 +9,13 @@ class Vertex:
 		self.color = 'white'
 		self.distance = 0
 		self.predecessor = None
+		self.discover = 0
+		self.finish = 0
 
 	def __str__(self):
-		return str(self.id) + ' adjacentTo ' + str([vertex.id for vertex in self.adjacentTo]) + '\ncolor' + self.color
+		return str(self.id) + ' adjacentTo ' \
+		+ str([vertex.id for vertex in self.adjacentTo]) + '\ncolor: ' + self.color \
+		+ '\ndiscover: ' + str(self.discover) + '\nfinish: ' + str(self.finish)
 
 	def addAdjacentVertex(self,vertex,weight = 0):
 		self.adjacentTo[vertex] = weight
@@ -74,12 +78,12 @@ g.addEdge(3,5,3)
 g.addEdge(4,0,1)
 g.addEdge(5,4,8)
 g.addEdge(5,2,1)
-
-
 #for v in g:
 #	for w in v.getAdjacencies():
 #		print("( %s , %s )" % (v.id, w.id))
 
+
+#bfs use a queue first in first out
 def bfs(graph,start):
 	vertexQueue = []
 	vertexQueue.append(start)
@@ -93,8 +97,57 @@ def bfs(graph,start):
 				vertexQueue.append(neighbor)
 		currentVertex.color = 'black'
 
+#bfs(g, g.getVertex(0))
+#for vertex in g:
+#	print vertex
 
-bfs(g, g.getVertex(0))
 
+		
+
+# dfs使用递归的方法 stack first in last out 
+class DFSGraph(Graph):
+
+	def __init__(self):
+		Graph.__init__(self)
+		self.time = 0
+
+	def dfs_visit(self,vertex):
+		vertex.color = 'grey'
+		self.time = self.time + 1
+		vertex.discover = self.time
+		for adjacency in vertex.getAdjacencies():
+			if adjacency.color == 'white':
+				adjacency.predecessor = vertex
+				self.dfs_visit(adjacency)
+		self.time = self.time + 1
+		vertex.color = 'black'
+		vertex.finish = self.time
+
+	def dfs(self):
+		#所有的vertex都访问一遍 防止有孤立点
+		for vertex in self:
+			if vertex.color == 'white':
+				self.dfs_visit(vertex)
+
+g = DFSGraph()
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+
+g.dfs()
 for vertex in g:
 	print vertex
+
+
+
+
+
+
+
+
